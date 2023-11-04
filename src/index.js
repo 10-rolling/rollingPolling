0bc9d6e0e3369fa8a1cdbcb55bf01848ee18ddbe
oauch <lgm9515@naver.com>
2023-11-04 21:01:10 +0900

    [#11] refactor: conflict 해결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import App from 'App';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Router from 'router/Router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
 import theme from 'styles/theme';
@@ -9,6 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <App />
+    <Router />
   </ThemeProvider>
 );
