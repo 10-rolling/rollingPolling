a220f52da0c389aa4d69668481d3863381c755f2
hanseulhee <3021062@gmail.com>
2023-11-04 16:56:41 +0900

    [#4] env: page 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
+import Router from 'router/Router';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Router />);
