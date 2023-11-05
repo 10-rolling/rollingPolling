888eb896990bb2fbc3657f63c11aa8eddf22124e
Soo NamGoong <rndtn6851@gmail.com>
2023-11-06 01:30:48 +0900

    [#13] style: buttonSize 관련 상수

---
+++ src/constants/buttonSize.js
@@ -0,0 +1,45 @@
+export const SIZES = {
+  large: {
+    padding: '0.875rem 1.5rem',
+    fontSize: '1.125rem',
+    fontWeight: '700',
+    borderRadius: '0.75rem',
+  },
+  small: {
+    padding: '0.4375rem 1rem',
+    fontSize: '1rem',
+    fontWeight: '400',
+    borderRadius: '0.375rem',
+  },
+};
+
+export const OUTLINED_SIZES = {
+  first: {
+    padding: '0.875rem 1rem',
+    fontSize: '1.125rem',
+    fontWeight: '700',
+    borderRadius: '0.75rem',
+    imgSize: '1.5rem',
+  },
+  second: {
+    padding: '0.5rem 1rem',
+    fontSize: '1rem',
+    fontWeight: '400',
+    borderRadius: '0.375rem',
+    imgSize: '1.5rem',
+  },
+  third: {
+    padding: '0.375rem 1rem',
+    fontSize: '1rem',
+    fontWeight: '500',
+    borderRadius: '0.375rem',
+    imgSize: '1.5rem',
+  },
+  fourth: {
+    padding: '0.125rem 1rem',
+    fontSize: '0.875rem',
+    fontWeight: '400',
+    borderRadius: '0.375rem',
+    imgSize: '1.25rem',
+  },
+};
