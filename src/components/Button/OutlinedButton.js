f1886230b77c0bcda3a39e3c3ed39c547979f332
Soo NamGoong <rndtn6851@gmail.com>
2023-11-06 01:17:49 +0900

    [#13] feat: OutlinedButton 구현

---
+++ src/components/Button/OutlinedButton.js
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import { OUTLINED_SIZES } from 'constants/buttonSize.js';
+import styled from 'styled-components';
+
+function OutlinedButton({ children, size, width }) {
+  return (
+    <StyledWrapper size={size} width={width}>
+      {children}
+    </StyledWrapper>
+  );
+}
+
+OutlinedButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  size: PropTypes.node.isRequired,
+  width: PropTypes.node.isRequired,
+};
+
+export default OutlinedButton;
+
+const StyledWrapper = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+
+  width: ${(props) => props.width};
+
+  padding: ${(props) => OUTLINED_SIZES[props.size].padding};
+  border: 1px solid ${({ theme }) => theme.colors.gray300};
+  border-radius: ${(props) => OUTLINED_SIZES[props.size].borderRadius};
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.gray900};
+
+  font-size: ${(props) => OUTLINED_SIZES[props.size].fontSize};
+  font-weight: ${(props) => OUTLINED_SIZES[props.size].fontWeight};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.gray100};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.gray300};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.gray100};
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${({ theme }) => theme.colors.gray500};
+    background-color: ${({ theme }) => theme.colors.white};
+  }
+`;
