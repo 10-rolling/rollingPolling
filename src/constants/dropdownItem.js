448e68b542f358eb11878273d42c95f0d6642a6e
Archer <sp38013@naver.com>
2023-11-06 01:28:27 +0900

    [#14]feat:dropdownitem 상수추가 및 화살표이미지추가

---
+++ src/constants/dropdownItem.js
@@ -0,0 +1,10 @@
+const DROPDOWN_ITEMS_DEFAULT = [
+  { id: 1, content: '임윤혁' },
+  { id: 2, content: '한슬희' },
+  { id: 3, content: '남궁수' },
+  { id: 4, content: '남궁수영' },
+];
+
+const PLACE_HOLDER_DEFAULT = 'placeholder';
+
+export { DROPDOWN_ITEMS_DEFAULT, PLACE_HOLDER_DEFAULT };
