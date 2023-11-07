58aaad29303ff26506b30279f5517716472df98f
oauch <lgm9515@naver.com>
2023-11-07 16:58:40 +0900

    [#33] feat: Editor 스타일드 컴포넌트로 변경

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,12 +1,19 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { styled } from 'styled-components';
 
 function Editor() {
   return (
     <>
-      <ReactQuill style={{ width: '720px', height: '260px' }} />
+      <EditorForm />
     </>
   );
 }
 
 export default Editor;
+
+const EditorForm = styled(ReactQuill)`
+  width: 720px;
+  height: 260px;
+  margin-bottom: 45px;
+`;
