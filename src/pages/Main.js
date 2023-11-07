d23e1259dd0f2ce46160f0e2dd83715c72bac4dc
Soo NamGoong <rndtn6851@gmail.com>
2023-11-07 23:05:50 +0900

    [#34] fix: pull을 위한 Main.js 커밋

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,5 @@
-import OutlinedButton from 'components/Button/OutlinedButton.js';
-
 function Main() {
-  return (
-    <>
-      <OutlinedButton size="smallest" width="10rem">
-        HI
-      </OutlinedButton>
-    </>
-  );
+  return <></>;
 }
 
 export default Main;
