d8576b1238ad8afa0cbde145b414418dbd974af4
oauch <lgm9515@naver.com>
2023-11-07 10:48:30 +0900

    [#33] feat: message 페이지 상수 추가

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+const FROM = 'From.';
+const PROFILE_IMG = '프로필 이미지';
+const RELATIONSHIP = '상대와의 관계';
+const ENTER_CONTENT = '내용을 입력해 주세요.';
+const FONT_SELECT = '폰트 선택';
+
+export { FROM, PROFILE_IMG, RELATIONSHIP, ENTER_CONTENT, FONT_SELECT };
