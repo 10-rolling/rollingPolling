6778791f6ea739c9249425134973c29020f75c97
oauch <lgm9515@naver.com>
2023-11-07 12:18:14 +0900

    [#33] feat: Label 컴포넌트 추가

---
+++ src/constants/labelSize.js
@@ -0,0 +1,18 @@
+import theme from 'styles/theme';
+
+export const LABEL_SIZES = {
+  large: {
+    fontSize: '24px',
+    fontWeight: '700',
+    lineHeight: '36px',
+    letterSpacing: '-0.24px',
+    color: theme.colors.black,
+  },
+  small: {
+    fontSize: '16px',
+    fontWeight: '400',
+    lineHeight: '26px',
+    letterSpacing: '-0.16px',
+    color: theme.colors.gray500,
+  },
+};
--- src/constants/message.js
+++ src/constants/message.js
@@ -3,5 +3,13 @@ const PROFILE_IMG = '프로필 이미지';
 const RELATIONSHIP = '상대와의 관계';
 const ENTER_CONTENT = '내용을 입력해 주세요.';
 const FONT_SELECT = '폰트 선택';
+const PROFILE_SELECT = '프로필 이미지를 선택해주세요!';
 
-export { FROM, PROFILE_IMG, RELATIONSHIP, ENTER_CONTENT, FONT_SELECT };
+export {
+  FROM,
+  PROFILE_IMG,
+  RELATIONSHIP,
+  ENTER_CONTENT,
+  FONT_SELECT,
+  PROFILE_SELECT,
+};
