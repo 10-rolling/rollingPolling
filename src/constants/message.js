d5a2f6deeaddfa19555c1212a8501153a252ae0a
oauch <lgm9515@naver.com>
2023-11-08 16:21:15 +0900

    [#33] feat: 상수 message 변경

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,6 @@
 // error
 const DROPDOWN_ERROR_MESSAGE_DEFAULT = 'Error Message';
-const INPUT_ERROR_MESSAGE_DEFAULT = 'Error Message';
+const INPUT_ERROR_MESSAGE_DEFAULT = '값을 입력해주세요.';
 
 const INPUT_PLACE_HOLDER_DEFAULT = 'placeholder';
 const URL_COPY = 'URL이 복사 되었습니다.';
@@ -12,6 +12,7 @@ const ENTER_CONTENT = '내용을 입력해 주세요.';
 const FONT_SELECT = '폰트 선택';
 const PROFILE_SELECT = '프로필 이미지를 선택해주세요!';
 const INPUT_NAME = '이름을 입력해 주세요.';
+const CREATE = '생성하기';
 
 export {
   DROPDOWN_ERROR_MESSAGE_DEFAULT,
@@ -25,4 +26,5 @@ export {
   FONT_SELECT,
   PROFILE_SELECT,
   INPUT_NAME,
+  CREATE,
 };
