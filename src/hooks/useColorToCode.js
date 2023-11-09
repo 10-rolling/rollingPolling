f2d1ea5e1783795f7b44e67b38e073daee6497e1
Archer <sp38013@naver.com>
2023-11-10 02:59:39 +0900

    [#56]refactor : useColorToCode hook 개선

--- src/hooks/useColorToCode.js
+++ src/hooks/useColorToCode.js
@@ -2,24 +2,29 @@ import { create } from 'zustand';
 import theme from 'styles/theme';
 
 const useColorToCode = create((set) => ({
-  color: theme.colors.orange200,
+  color: '',
   setColor: (colorName) => {
     let colorCode = '';
     switch (colorName) {
       case 'orange':
         colorCode = theme.colors.orange200;
+        set({ color: colorCode });
         break;
       case 'purple':
         colorCode = theme.colors.purple200;
+        set({ color: colorCode });
         break;
       case 'blue':
         colorCode = theme.colors.blue200;
+        set({ color: colorCode });
         break;
       case 'green':
         colorCode = theme.colors.green200;
+        set({ color: colorCode });
         break;
+      default:
+        set({ color: theme.colors.orange200 });
     }
-    set({ color: colorCode });
   },
 }));
 
