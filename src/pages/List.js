e8bbdbc8c87ad90fbf772dda68a26d3c7f5f113e
hanseulhee <3021062@gmail.com>
2023-11-10 12:17:29 +0900

    [#52] feat: 나도 만들어보기 링크 추가

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import PrimaryButton from 'components/Button/PrimaryButton';
+import Nav from 'components/Nav/Nav';
+import PaperCategory from 'components/Text/PaperCategory';
+
+
 function List() {
-  return;
+  return (
+    <>
+      <Nav />
+      <PaperCategory title="인기 롤링 페이퍼" emogi="🔥" />
+      <PaperCategory title="최근에 만든 롤링 페이퍼" emogi="⭐️" />
+      <Link to="/post">
+        <PrimaryButton size="large" content="나도 만들어보기" width="280px" />
+      </Link>
+    </>
+  );
 }
 
 export default List;
