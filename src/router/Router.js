9b0d798cf972c11d88fab23a038543c170e7853c
oauch <lgm9515@naver.com>
2023-11-10 17:27:48 +0900

    [#68] refactor: Router 설정 및 POST 페이지 스타일 변경

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,9 +3,9 @@ import NotFound from 'NotFound';
 import List from 'pages/List';
 import Main from 'pages/Main';
 import Post from 'pages/Post';
-import PostList from 'pages/PostList';
 import SendMessage from 'pages/SendMessage';
 import Root from 'router/Root';
+import PostList from 'pages/PostList';
 
 function Router() {
   const router = createBrowserRouter([
@@ -14,14 +14,17 @@ function Router() {
       element: <Root />,
       errorElement: <NotFound />,
       children: [
-        { path: '/', element: <Main /> },
+        { index: true, element: <Main /> },
         { path: '/list', element: <List /> },
         {
           path: '/post',
-          element: <Post />,
-          children: [{ path: ':id/message', element: <SendMessage /> }],
+          element: <Root />,
+          children: [
+            { index: true, element: <Post /> },
+            { path: ':id/', element: <PostList /> },
+            { path: ':id/message', element: <SendMessage /> },
+          ],
         },
-        { path: 'post/:id/', element: <PostList /> },
       ],
     },
   ]);
