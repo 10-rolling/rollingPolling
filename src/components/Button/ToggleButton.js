c9cd53c3dc2671e8a46a32eeef4c9e7339fb6233
Archer <sp38013@naver.com>
2023-11-11 14:27:19 +0900

    fix : ToggleButton Styled-components 변수명 변경

--- src/components/Button/ToggleButton.js
+++ src/components/Button/ToggleButton.js
@@ -24,15 +24,15 @@ function ToggleButton({ onClick }) {
   };
 
   return (
-    <StyledWrapper isActive={isActive} onClick={handleToggle}>
+    <StyledWrapper $isActive={isActive} onClick={handleToggle}>
       <StyledToggleButton
-        isActive={colorButtonActive}
+        $isActive={colorButtonActive}
         onClick={() => handleButtonClick(1)}
       >
         컬러
       </StyledToggleButton>
       <StyledToggleButton
-        isActive={imageButtonActive}
+        $isActive={imageButtonActive}
         onClick={() => handleButtonClick(2)}
       >
         이미지
@@ -64,9 +64,9 @@ const StyledToggleButton = styled.button`
   padding: 8px 16px;
   outline: none;
   border-radius: 6px;
-  border: 2px solid ${({ isActive }) => (isActive ? '#861DEE' : '#FFFFFF')};
+  border: 2px solid ${({ $isActive }) => ($isActive ? '#861DEE' : '#FFFFFF')};
   background-color: ${theme.colors.white};
-  color: ${({ isActive }) => (isActive ? '#861DEE' : '#181818')};
+  color: ${({ $isActive }) => ($isActive ? '#861DEE' : '#181818')};
 
   font-size: 1rem;
   font-weight: 700;
