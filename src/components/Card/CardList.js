0f30ea6636ae5a5988cab2ea9721d5a888130a77
hanseulhee <3021062@gmail.com>
2023-11-12 23:33:17 +0900

    [#52] feat: 카드 슬라이드 구현

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -1,30 +1,67 @@
+import { Link } from 'react-router-dom';
 import Emoji from 'components/Badge/Emoji';
+import { CARD_THEME } from 'constants/cardTheme';
 import styled from 'styled-components';
 import theme from 'styles/theme';
 
-function SelectPaperBg({ color, img, name, num, emoji, count }) {
+function CardList({ id, color, img, name, num, reactions }) {
   return (
-    <StyledWrapper color={color}>
-      <StyledImgWrapper>
-        <img src={img} alt="도형 이미지" />
-      </StyledImgWrapper>
-      <StyledName>To. {name}</StyledName>
+    <StyledLink to={`/post/${id}`}>
+      {img ? (
+        <StyledWrapper>
+          <StyledImgWrapper>
+            <img src={img} alt="도형 이미지" />
+          </StyledImgWrapper>
 
-      <StyledNumWrapper>
-        <StyledNum>{num}</StyledNum>명이 작성했어요!
-      </StyledNumWrapper>
+          <StyledContentWrapper>
+            <StyledName>To. {name}</StyledName>
 
-      <StyledBottomWrapper>
-        <StyledLabelWrapper />
-        <StyledEmojiWrapper>
-          <Emoji emoji={emoji} count={count} />
-        </StyledEmojiWrapper>
-      </StyledBottomWrapper>
-    </StyledWrapper>
+            <StyledNumWrapper>
+              <StyledNum>{num}</StyledNum>명이 작성했어요!
+            </StyledNumWrapper>
+          </StyledContentWrapper>
+
+          <StyledBottomWrapper>
+            <StyledLabelWrapper />
+            <StyledEmojiWrapper>
+              {reactions.map((item) => (
+                <Emoji emoji={item.emoji} count={item.count} key={item.id} />
+              ))}
+            </StyledEmojiWrapper>
+          </StyledBottomWrapper>
+        </StyledWrapper>
+      ) : (
+        <StyledWrapper color={color}>
+          <StyledColorImgWrapper>
+            <img src={CARD_THEME[color]?.img} alt="도형 이미지" />
+          </StyledColorImgWrapper>
+
+          <StyledContentWrapper>
+            <StyledName>To. {name}</StyledName>
+
+            <StyledNumWrapper>
+              <StyledNum>{num}</StyledNum>명이 작성했어요!
+            </StyledNumWrapper>
+          </StyledContentWrapper>
+          <StyledBottomWrapper>
+            <StyledLabelWrapper />
+            <StyledEmojiWrapper>
+              {reactions.map((item) => (
+                <Emoji emoji={item.emoji} count={item.count} key={item.id} />
+              ))}
+            </StyledEmojiWrapper>
+          </StyledBottomWrapper>
+        </StyledWrapper>
+      )}
+    </StyledLink>
   );
 }
 
-export default SelectPaperBg;
+export default CardList;
+
+const StyledLink = styled(Link)`
+  all: unset;
+`;
 
 const StyledWrapper = styled.div`
   position: relative;
@@ -33,14 +70,20 @@ const StyledWrapper = styled.div`
 
   width: 220px;
   height: 220px;
-  padding: 20px;
   border-radius: 16px;
   border: 1px;
-  background-color: ${(props) => props.color};
+  background-color: ${(props) => CARD_THEME[props.color]?.color};
   overflow: hidden;
 `;
 
 const StyledImgWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const StyledColorImgWrapper = styled.div`
   position: absolute;
   right: -33px;
   bottom: 0;
@@ -50,6 +93,11 @@ const StyledImgWrapper = styled.div`
 const StyledName = styled.span`
   font-size: 1.3rem;
   font-weight: ${theme.fontWeight.bold};
+`;
+
+const StyledContentWrapper = styled.div`
+  z-index: 1;
+  padding: 20px 20px 0 20px;
 `;
 
 const StyledNumWrapper = styled.div`
@@ -63,6 +111,8 @@ const StyledNum = styled.strong`
 const StyledBottomWrapper = styled.div`
   position: relative;
   margin-top: 80px;
+  padding: 20px;
+  padding-top: 0px;
 `;
 
 const StyledLabelWrapper = styled.div`
