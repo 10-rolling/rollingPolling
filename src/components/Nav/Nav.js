33b75a6fc119108f738ff19a7c5807e9c3402e94
oauch <lgm9515@naver.com>
2023-11-13 11:05:27 +0900

    [#86] feat: Nav 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,20 +3,20 @@ import OutlinedButton from 'components/Button/OutlinedButton';
 import logo from 'assets/images/logo.png';
 import styled from 'styled-components';
 
-function Nav() {
+function Nav({ hide }) {
   return (
     <StyledWrapper>
       <Link to="/">
         <img src={logo} alt="로고" />
       </Link>
-      <Link to="/post">
+      <StyledLink to="/post" hide={hide}>
         <OutlinedButton
           content="롤링 페이퍼 만들기"
           size="smallest"
           width="150px"
           height="35px"
         />
-      </Link>
+      </StyledLink>
     </StyledWrapper>
   );
 }
@@ -29,6 +29,13 @@ const StyledWrapper = styled.nav`
   justify-content: space-around;
   align-items: center;
   width: 100%;
-  padding: 10px 0px;
+  padding: 10px 15%;
   border-bottom: 1px solid rgba(237, 237, 237, 1);
+`;
+
+const StyledLink = styled(Link)`
+  position: absolute;
+  display: ${(props) => (props.hide ? 'none' : '')};
+  right: 0;
+  padding-right: 15%;
 `;
