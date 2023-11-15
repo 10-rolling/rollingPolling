6189bb42a4c30fadccdbb514414f4530d5d608b4
hanseulhee <3021062@gmail.com>
2023-11-15 15:30:47 +0900

    fix: logo svg 변경

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import OutlinedButton from 'components/Button/OutlinedButton';
 import PrimaryButton from 'components/Button/PrimaryButton';
 import { deleteAll } from 'libs/api';
-import logo from 'assets/images/logo.png';
+import logo from 'assets/icons/logo.svg';
 import styled from 'styled-components';
 import { onMobile, onTablet } from 'styles/mediaQuery';
 
