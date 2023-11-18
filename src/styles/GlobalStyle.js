61388a77f513b94d81082f001fce18fb8a070241
oauch <lgm9515@naver.com>
2023-11-18 20:52:35 +0900

    [#108] refactor: 글로벌 스타일 폰트 이름 변경

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Noto Sans KR", sans-serif;
+    font-family: "NotoSansKR", sans-serif;
     font-weight: ${theme.fontWeight.normal};
     &::-webkit-scrollbar {
     display: none; 
